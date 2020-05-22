6897e5d3f377c5ff216cbfcdcee1292ba90cbb28
Coralie Helm <helm.coralie@gmail.com>
2020-05-22 17:34:57 -0400

    Added new function with multiple values

--- introduction_to_functions.py
+++ introduction_to_functions.py
@@ -2,9 +2,9 @@
 #May 22 2020
 
 #INTRODUCTION TO FUNCTIONS
+############################################
 
 #8/11 Returns
-
 #Create a function that calcualtes_age
 
 def calculate_age(current_year, birth_year):
@@ -18,3 +18,12 @@ print("I am ", my_age, " years old and my dad is ", dads_age, " old.")
 
 
 
+#9/11 Multipile Return Values
+# Create a function with two return values
+
+def get_boundaries(target, margin):
+  low_limit = target - margin
+  high_limit = margin + target
+  return low_limit, high_limit
+
+low, high = get_boundaries(100,20)
