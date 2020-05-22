e79a284559968d7670afc436e98155bef463d7a4
Coralie Helm <helm.coralie@gmail.com>
2020-05-22 17:28:40 -0400

    Introduction to Functions

---
+++ introduction_to_functions.py
@@ -0,0 +1,20 @@
+#CodeAcademy
+#May 22 2020
+
+#INTRODUCTION TO FUNCTIONS
+
+#8/11 Returns
+
+#Create a function that calcualtes_age
+
+def calculate_age(current_year, birth_year):
+  age = current_year - birth_year
+  return age
+
+my_age = calculate_age(2049, 1993)
+dads_age = calculate_age(2049,1953)
+
+print("I am ", my_age, " years old and my dad is ", dads_age, " old.")
+
+
+
